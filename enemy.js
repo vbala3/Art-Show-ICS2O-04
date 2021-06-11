1c47622522fa3f6140174fbba232160a393de684
vbala3 <83592667+vbala3@users.noreply.github.com>
2021-06-11 10:01:20 -0400

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,20 +1,23 @@
-//This is the code for the common enemy
+//This is the code for the common and tougher enemy
 function Enemy(x, y) {
   this.x = x;
   this.y = y;
   this.r = 20;
-  this.dec = 20;
+  this.dec = 0;
   this.speed = 1;
   this.rid = false;
+  this.out = false;
 
   //Destroys the enemy when hit enough times completely and increases the score
   this.oust = function () {
-    if (this.r <= 0) {
+    if (this.r <= 0 || this.rTough) {
       this.rid = true;
+    } else if (this.y >= 400 + this.r) {
+      this.out = true;
     }
   };
 
-  //Lowers the enemies size and health points 
+  //Lowers the enemies size and health points
   this.shrink = function () {
     if (this.r <= 0) {
       return false;
@@ -22,7 +25,7 @@ function Enemy(x, y) {
       this.r = this.r - this.dec;
     }
   };
-
+  
   //Checks if the player's character has collided with the enemy
   this.hits = function (player) {
     let d = dist(this.x, this.y, player.x, player.y);
@@ -35,16 +38,23 @@ function Enemy(x, y) {
       return false;
     }
   };
-
+  
   //Allows the enemy to move downwards
   this.move = function () {
     this.y = this.y + this.speed;
   };
-
+  
   //Creates the enemy
   this.show = function () {
     noStroke();
     fill("purple");
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  };
+  
+  //Creates the tougher enemy
+  this.showTough = function () {
+    noStroke();
+    fill("pink");
     ellipse(this.x, this.y, this.r * 2, this.r * 2);
   };
 }
